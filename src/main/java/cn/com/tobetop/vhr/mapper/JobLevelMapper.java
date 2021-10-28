b1ae3d3566d30ed188af5a09ea8379227a1debc0
cashlu <cashlu@yeah.net>
2021-10-29 03:36:43 +0800

    修改了JobLevel相关的变量名称Bug

---
+++ src/main/java/cn/com/tobetop/vhr/mapper/JobLevelMapper.java
@@ -0,0 +1,17 @@
+package cn.com.tobetop.vhr.mapper;
+
+import cn.com.tobetop.vhr.entity.JobLevel;
+
+public interface JobLevelMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(JobLevel record);
+
+    int insertSelective(JobLevel record);
+
+    JobLevel selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(JobLevel record);
+
+    int updateByPrimaryKey(JobLevel record);
+}
